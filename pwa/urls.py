2a21ad0dab66c24e36797362cfaa9e5429aa6dac
Tommy D <52969440+Dunit05@users.noreply.github.com>
2024-04-28 16:55:45 -0400

    Delete pwa directory

--- pwa/urls.py
+++
@@ -1,8 +0,0 @@
-# pwa/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("analyze_picture/", views.analyze_picture, name="analyze_picture"),
-]
